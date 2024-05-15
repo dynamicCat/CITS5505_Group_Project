03bb239178c48ead823f9d1a0125fea22a8e0295
dynamicCat <96351304+dynamicCat@users.noreply.github.com>
2024-05-16 02:38:19 +0800

    fix bug
    fix problem that the error message on login page is not currently working.

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-class Config:
-    load_dotenv()
-    SECRET_KEY = os.getenv('SECRET_KEY', 'CITS')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
