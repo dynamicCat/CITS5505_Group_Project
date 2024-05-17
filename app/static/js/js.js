94f3c7b1db142a4461b50838c45d36c00fc8d75b
Zihan Peng <zpen0004@student.monash.edu>
2024-05-18 03:13:30 +0800

    Add mobile layout to header

--- app/static/js/js.js
+++ app/static/js/js.js
@@ -18,3 +18,22 @@ function switchTab(tabId) {
 
     document.getElementById(tabId).classList.add('active');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    function toggleOpen() {
+        document.querySelector('body').classList.toggle('open');
+        for (var i = 0; i < links.length; i++) {
+            if (document.querySelector('body').classList.contains('open')) {
+                links[i].style.transitionDelay = i * 0.25 + 's';
+            } else {
+                links[i].style.transitionDelay = '0s';
+            }
+        }
+    }
+
+    document.querySelector('#burger').addEventListener('click', toggleOpen);
+    var links = document.querySelectorAll('header nav a');
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', toggleOpen);
+    }
+});
