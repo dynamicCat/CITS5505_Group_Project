8ee0486a3bc83f4b7d0ff980ae029863c5af4d77
Zihan Peng <zpen0004@student.monash.edu>
2024-05-18 20:03:12 +0800

    When the user logs in, part of the introductory interface changes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,13 @@
 from flask import render_template
 from . import bp as main_bp
+from flask_login import current_user
 
 @main_bp.route('/')
 def home():
+    user = current_user
+    if user.is_authenticated:
+        return render_template('main/introductory.html', user=user)
+        
     return render_template('main/introductory.html')
+
+
